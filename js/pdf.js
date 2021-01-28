b4ebbca4de6383ee876773a22c69ab12a9da3e06
LJaku <59481853+LJ59@users.noreply.github.com>
2021-01-28 20:45:13 +0100

    Add files via upload

--- js/pdf.js
+++ js/pdf.js
@@ -11,10 +11,10 @@ window.onload = function() {
             .then(function(group) {
                 kendo.drawing.pdf.saveAs(group, "le_nord_aide_les_aidants.pdf")
             });
-        kendo.pdf.defineFont({
-            "Arial": "https://cdn.kendostatic.com/2017.2.621/styles/fonts/Arial/Arial.ttf"
-                //"DejaVu Sans": "http://cdn.kendostatic.com/2017.2.621/styles/fonts/DejaVu/DejaVuSans.ttf"
-        });
+        // kendo.pdf.defineFont({
+        //     "Arial": "https://cdn.kendostatic.com/2017.2.621/styles/fonts/Arial/Arial.ttf"
+        //         "DejaVu Sans": "http://cdn.kendostatic.com/2017.2.621/styles/fonts/DejaVu/DejaVuSans.ttf"
+        // });
 
     })
 
