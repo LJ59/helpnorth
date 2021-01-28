8a6cdae99b2bed70f474abd0787cc56c212fdc8b
LJaku <59481853+LJ59@users.noreply.github.com>
2021-01-28 15:31:32 +0100

    Add files via upload

--- js/pdf.js
+++ js/pdf.js
@@ -5,14 +5,15 @@ window.onload = function() {
                 paperSize: "A4",
                 margin: { top: "1cm", bottom: "1cm" },
                 scale: 0.5,
-                pagebreak: { mode: 'avoid-all', before: '#engagement1' },
+                template: $("#page-template").html(),
                 height: 500
             })
             .then(function(group) {
                 kendo.drawing.pdf.saveAs(group, "le_nord_aide_les_aidants.pdf")
             });
         kendo.pdf.defineFont({
-            "Arial": "http://cdn.kendostatic.com/2017.2.621/styles/fonts/Arial/Arial.ttf"
+            // "Arial": "https://cdn.kendostatic.com/2017.2.621/styles/fonts/Arial/Arial.ttf"
+            "DejaVu Sans": "http://cdn.kendostatic.com/2017.2.621/styles/fonts/DejaVu/DejaVuSans.ttf"
         });
 
     })
